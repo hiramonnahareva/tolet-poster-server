85fbc7b46174a590dc511eb2e89435bea114e0ee
hiramonnahareva <hiramonnahareva@gmail.com>
2025-08-02 19:08:15 +0600

    Fix build error

--- models/Pin.js
+++ models/Pin.js
@@ -3,4 +3,4 @@ const PinSchema = new mongoose.Schema({
   userId: String, // Replace with ObjectId if you add auth
   postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
 });
-module.exports = mongoose.model('Pin', PinSchema); 
+module.exports = mongoose.model('Pin', PinSchema);  
