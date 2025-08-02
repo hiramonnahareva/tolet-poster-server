062fcab7fa7bd9756932b8202b6c4033b2a6cb2c
hiramonnahareva <hiramonnahareva@gmail.com>
2025-08-03 02:19:15 +0600

    Debug

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/posts", postsRouter);
-app.use("/api/pins", pinsRouter);   
+app.use("/api/pins", pinsRouter);      
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })  
+mongoose.connect(process.env.MONGO_URI)  
   .then(() => {
     console.log("✅ MongoDB connected")
     app.listen(PORT, () => {
